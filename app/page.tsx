4005e6d87f7e5422f3e337c8cb43c30dd6bf0478
Artur <artur.talimonchyk@gmail.com>
2025-04-07 17:00:05 +0200

    got back to the version without clerk. Just to implement the Oauth

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,23 @@
+import Link from "next/link";
 import Button from "./_components/Button/Button";
 import styles from '@/app/page.module.scss'
-import { SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
-import { auth } from "@clerk/nextjs/server";
+import User from "./_components/User/User";
+import { getCurrentUser } from "./_auth/currentUser";
 export default async function Home() {
-  const { userId } = await auth()
-  if (userId) return <UserButton />
+  const fullUser = await getCurrentUser({ withFullUser: true, redirectIfNotFound: true })
+  if (fullUser) return <User user={fullUser} />
   return (
-    <div className={styles.authorize}>
-      <div className="px-4 py-1 bg-red-400 rounded-2xl text-2xl cursor-pointer">
-        <SignInButton />
-      </div>
-      <div className="px-4 py-1 bg-red-400 rounded-2xl text-2xl cursor-pointer">
-        <SignUpButton />
-      </div>
+    <div className={styles.authorize}> 
+      <Button>
+        <Link href='/sign-in'>
+          Sign in
+        </Link>
+      </Button>
+      <Button>
+        <Link href='/sign-up'>
+          Sign up
+        </Link>
+      </Button>
     </div>
   );
 }
