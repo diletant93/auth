523bfa0d0b7363bbd432dee57bb9e49f0a8551bd
Artur <artur.talimonchyk@gmail.com>
2025-04-09 16:11:49 +0200

    fixed for deploy

--- app/_providers/Providers.tsx
+++ app/_providers/Providers.tsx
@@ -2,16 +2,12 @@
 import { Provider } from "react-redux"
 import store from "../store/store";
 import { Toaster } from "sonner";
-import { ClerkProvider } from "@clerk/nextjs";
 
 export default function Providers({ children }: Readonly<{children:React.ReactNode}>) {
   return (
-    <ClerkProvider> 
       <Provider store={store}>
         <Toaster />
         {children}
       </Provider>
-    </ClerkProvider>
-
   );
 }
